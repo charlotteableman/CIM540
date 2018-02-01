1c7ef2ac091030318f4e8a3d53fd5fc6ed36a821
Charlotte Ableman <cpa14@miami.edu>
2018-02-01 14:05:12 -0500

    Hw2 upload

--- hw/p5/HW2/sketch.js
+++ hw/p5/HW2/sketch.js
@@ -1,9 +1,9 @@
-var centerX = 200;
-var centerY = 200;
+var centerX = 100;
+var centerY = 100;
 
 function setup() {
   // put setup code here
-  createCanvas(400,400);
+  createCanvas(800,800);
   background(255);
 }
 
@@ -16,20 +16,35 @@ function draw() {
   stroke (0);
   strokeWeight(1);
   fill("white");
-  rect(centerX -150,centerY -100,100,100);
-  ellipse(centerX -100,centerY -50,50,50);
-  rect(centerX +25,centerY -100,100,100);
-  ellipse(centerX +75,centerY -50,50,75);
+  ellipse(centerX -25, centerY -100, 500, 500);
+  ellipse(centerX -100,centerY -100,100,100);
+  ellipse(centerX -100,centerY -75,50,50);
+  ellipse(centerX +50,centerY -100,100,100);
+  ellipse(centerX +25,centerY -100,50,50);
+
+  stroke (0);
+  strokeWeight(1);
+  fill ("black");
+  ellipse(centerX -150,centerY -300, 200, 175);
+  ellipse(centerX +100,centerY -300, 200, 175);
 
   stroke(125);
   strokeWeight(5);
-  point(centerX, centerY);
+  point(centerX -25, centerY -20);
 
   stroke(255, 0, 0);
-  line(centerX -50,centerY +50, centerX +50,centerY +50);
+  line(centerX -100,centerY +50, centerX +50,centerY +50);
 
   noStroke();
-  fill("blue");
-  triangle(centerX,centerY,centerX +25, centerY +25, centerX -25, centerY +25);
+  fill("red");
+  ellipse(centerX -25, centerY +50, 75, 25);
 
+  noStroke();
+  fill("black");
+  ellipse(centerX -25, centerY -25, 75, 25);
+
+  noStroke();
+  fill("pink");
+  ellipse(centerX +60, centerY -25, 75, 25);
+  ellipse(centerX -110, centerY -25, 75, 25); 
 }
